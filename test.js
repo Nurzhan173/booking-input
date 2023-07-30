d44ff0b94e64f50be986c7ae5ab277831e180d0f
nurzhann173 <nurzhann173@gmail.com>
2023-07-30 15:17:48 +0600

    added tooltip

--- test.js
+++ test.js
@@ -1,19 +1,27 @@
 const requester = require('nurzhan-package/requester');
 const debounce = require('nurzhan-package/debounce');
-import { Skeleton } from './components/Skeleton/skeleton';
+const Skeleton = require('nurzhan-package/components/Skeleton/skeleton');
+const Modal = require('nurzhan-package/components/Modal/modal');
+const Tooltip = require('nurzhan-package/components/Tooltip/tooltip');
+
 import { BASE_URL, API_KEY } from './constants/constants';
 
 window.xhr = new XMLHttpRequest();
 
 const appContainer = document.getElementById('app');
 
-const skeletonComponent = new Skeleton();
-appContainer.appendChild(skeletonComponent.render());
+const SkeletonComponent = new Skeleton();
+const ModalComponent = new Modal();
+const TooltipComponent = new Tooltip();
+
+appContainer.appendChild(SkeletonComponent.render());
+appContainer.appendChild(ModalComponent.render());
 
 const cards = document.getElementById("cards");
 const input = document.getElementById("input");
-const cross = document.getElementById('cross');
 const skeleton = document.querySelector('.skeleton');
+
+window.modal = document.getElementById("modal");
 
 async function renderCards(response) {
 
@@ -56,15 +64,64 @@ async function renderCards(response) {
   });
 }
 
+const getAuthors = (arr) => {
+  let str = "";
+
+  for(let item of arr){
+    str += `${item}, `
+  }
+
+  return str;
+}
+
 const renderBook = (response) => {
+
+  appContainer.appendChild(TooltipComponent.render());
 
   const content = document.getElementById('modal-content');
   content.innerHTML = "";
 
+  const image = document.createElement("img");
+  const detail = document.createElement("div");
   const title = document.createElement("p");
-  title.textContent = response.volumeInfo.title;
+  const authors = document.createElement("p");
+  const description = document.createElement('p');
+  const country = document.createElement('p');
+  const readLink = document.createElement('button');
 
-  content.append(title);
+  const tooltip = document.getElementById('tooltip');
+  const tooltipText = document.getElementById('tooltipText');
+
+  tooltipText.textContent = response.volumeInfo?.title;
+
+  image.src = response.volumeInfo.imageLinks?.medium || './assets/bookMock.png';
+
+  image.width = 300;
+  image.height = 400;
+
+  title.textContent = response.volumeInfo?.title;
+  authors.textContent = 'Authors: ' + getAuthors(response.volumeInfo?.authors);
+  country.textContent = 'Country: ' + response.accessInfo.country;
+  description.textContent = 'Description: ' + response.volumeInfo?.description;
+  readLink.textContent = 'Read';
+
+  readLink.addEventListener('click', function () {
+    const url = response.accessInfo.webReaderLink;
+    window.open(url, '_blank');
+  })
+
+  detail.className = 'book-detail';
+
+  tooltip.appendChild(title);
+  detail.append(tooltip);
+  detail.append(authors);
+  detail.append(country);
+  detail.append(readLink);
+  detail.append(description);
+
+  content.append(image);
+  content.append(detail);
+
   modal.appendChild(content);
 
   modal.style.display = "block";
